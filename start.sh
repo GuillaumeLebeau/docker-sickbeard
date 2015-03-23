c1924d6b3b76a792bb0d3672b6217c6d6d67c1c2
Guillaume Lebeau <guillaume.lebeau@outlook.com>
2015-03-23 18:04:43 +0100

    Update config path

--- start.sh
+++ start.sh
@@ -1,18 +1,18 @@
 #! /bin/sh
 
 cd /sickbeard
-if [ -f /config/config.ini ]
+if [ -f /volumes/config/config.ini ]
 then
 	rm -rf /sickbeard/config.ini
 	rm -rf /sickbeard/sickbeard.db
 #	rm -rf /sickbeard/autoprocesstv/autoProcessTV.cfg
 else
-	mv -f /sickbeard/config.ini /config/config.ini
-	mv -f /sickbeard/sickbeard.db /config/sickbeard.db
-#	mv -f /sickbeard/autoprocesstv/autoProcessTV.cfg /config/autoProcessTV.cfg
+	mv -f /sickbeard/config.ini /volumes/config/config.ini
+	mv -f /sickbeard/sickbeard.db /volumes/config/sickbeard.db
+#	mv -f /sickbeard/autoprocesstv/autoProcessTV.cfg /volumes/config/autoProcessTV.cfg
 fi
-ln -sf /config/config.ini /sickbeard/
-ln -sf /config/sickbeard.db /sickbeard/sickbeard.db
-#ln -sf /config/autoProcessTV.cfg sickbeard/autoprocesstv/autoProcessTV.cfg
+ln -sf /volumes/config/config.ini /sickbeard/
+ln -sf /volumes/config/sickbeard.db /sickbeard/sickbeard.db
+#ln -sf /volumess/config/autoProcessTV.cfg sickbeard/autoprocesstv/autoProcessTV.cfg
 
 exec /usr/bin/python SickBeard.py
